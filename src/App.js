300f0a73680b6075a7f8e805429ab7c6149f9089
TEST-AUTOMIOS <144019951+TEST-AUTOMIOS@users.noreply.github.com>
2023-09-07 19:49:09 +0530

    features added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import './App.css';
-// import Home from './Home';
-import Navbar from './Navbar';
-// In your index.js or App.js file, add the following import
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Service from './Our_service';
-import Home from './Home';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
-
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Service from "./components/Service";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+// import ScrollToTopButton from './components/ScrollTopButton';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
-    <div >
-    <Navbar/>
-    <Home/>
-    
-
-    </div>
+    <BrowserRouter>
+      <div className="container-fluid">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Service />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact_form" element={<ContactForm/>}/>
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
